4584c865ab61be0a61c62cdbbd5733c18ddd0de1
Kento Wada <59147620+KentoWada@users.noreply.github.com>
2026-07-27 20:12:37 +0300

    Fix variable name typo in loading_data.py

--- src/loading_data.py
+++ src/loading_data.py
@@ -18,7 +18,7 @@ files_to_tables = {
 }
 
 for file_name, table_name in files files_to_tables.items():
-    file_path =  os.path.join(data_dir,filename)
+    file_path =  os.path.join(data_dir,file_name)
     df = pd.read_csv(file_path)
     df.to_sql(table_name, con=engine, if_exists="append", index=False)
     print(f"Loaded {len(df)} rows into {table_name}")
